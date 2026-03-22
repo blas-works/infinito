c3591c25ab2eb9479a2f1e53941250fdbf727a7b
Luis Torres <torrescereno@gmail.com>
2026-03-22 00:18:46 -0300

    fix: generar iconos linux

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,17 +1,24 @@
 import { resolve } from 'path'
-import { defineConfig } from 'electron-vite'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import nodeResolve from '@rollup/plugin-node-resolve'
 
 export default defineConfig({
   main: {
+    plugins: [
+      externalizeDepsPlugin({ exclude: ['drizzle-orm'] }),
+      nodeResolve({ exportConditions: ['node'] })
+    ],
     build: {
       rollupOptions: {
         external: ['better-sqlite3']
       }
     }
   },
-  preload: {},
+  preload: {
+    plugins: [externalizeDepsPlugin()]
+  },
   renderer: {
     resolve: {
       alias: {
